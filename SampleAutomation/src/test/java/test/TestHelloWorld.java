060cf6ba764441fe08dd6c466d3651f5ca41ff0b
INDU M <INDU M@DESKTOP-IBDLSNL>
2022-09-05 20:48:32 +0530

    First commit

---
+++ SampleAutomation/src/test/java/test/TestHelloWorld.java
@@ -0,0 +1,16 @@
+package test;
+
+import org.junit.Test;
+
+public class TestHelloWorld {
+
+	
+		@Test
+		public void secondTest(){
+		
+			System.out.println("Hi My 2nd maven pgm");
+		}
+
+	
+
+}
